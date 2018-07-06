a15175c4a3e594bd321e66c8ea43c7c799314802
[chenamin] <[1432815163@qq.com]>
2018-07-06 18:15:24 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
@@ -8,6 +9,7 @@ import '@/assets/css/theme-default/index.css';
 import '@/assets/css/all.css';
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
